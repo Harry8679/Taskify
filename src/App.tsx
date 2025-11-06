766c2ba7c5b6890375d0245dfa468ff489045d3e
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-11-06 12:00:41 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import './App.css'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className="min-h-screen bg-gray-200 flex flex-col items-center p-4">
+      <h1 className='text-3xl font-bold mb-4'>Taskify</h1>
+      <form className='flex gap-2 mb-4'>
+        <input className='border p-2 rounded w-64' type="text" placeholder='Ajouter une tâche' />
+        <button className='bg-blue-500 text-white px-4 py-2 rounded'>Ajouter</button>
+      </form>
+    </div>
   )
 }
 
