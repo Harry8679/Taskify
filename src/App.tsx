9046119d1621427dff52cad587a7d80a514451bc
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-11-06 15:02:25 +0100

    Delete task

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 interface Task {
@@ -9,25 +9,42 @@ interface Task {
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [title, setTitle] = useState("");
+
+  const addTask = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!title.trim()) return;
+    const newTask: Task = { id: Date.now(), title, completed: false };
+    setTasks([...tasks, newTask]);
+    setTitle("");
+  };
+
+  const deleteTask = (id: number) => {
+    setTasks(tasks.filter((t) => t.id !== id));
+  }
   return (
     <div className="min-h-screen bg-gray-200 flex flex-col items-center p-4">
       <h1 className='text-3xl font-bold mb-4'>Taskify</h1>
-      <form className='flex gap-2 mb-4'>
-        <input className='border p-2 rounded w-64' type="text" placeholder='Ajouter une tâche' />
+      <form className='flex gap-2 mb-4' onSubmit={addTask}>
+        <input className='border p-2 rounded w-64' type="text" placeholder='Ajouter une tâche' 
+          value={title} onChange={(e) => setTitle(e.target.value)}/>
         <button className='bg-blue-500 text-white px-4 py-2 rounded'>Ajouter</button>
       </form>
 
       {/* Liste des taches */}
       <ul className='w-72'>
         {tasks.length === 0 ? (
-          <p className='text-gray-500 text-center italic'>Aucun tache pour le moment.</p>
+          <p className='text-gray-500 text-center italic'>Aucune tache pour le moment.</p>
         ): (
           tasks.map((t) => (
           <li key={t.id} className='flex justify-between bg-white p-2 mb-2 rounded shadow'>
             <span className={`cursor-pointer ${t.completed ? 'line-through text-gray-400': ''}`}>
               {t.title}
             </span>
-            <button className='bg-red-500 hover:bg-red-700 text-white font-bold w-6 h-6 rounded items-center shadow-sm'>x</button>
+            <button onClick={() => deleteTask(t.id)} 
+            className='bg-red-500 hover:bg-red-700 text-white font-bold w-6 h-6 rounded items-center shadow-sm'>
+              x
+            </button>
           </li>
         ))
         )}
